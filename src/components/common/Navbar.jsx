e76a19130c1cf2657cb0db474a240df266b99291
Shobhan Bhagwati <shobhanbhagwati2004@gmail.com>
2025-10-13 16:32:19 +0530

    removed dropdown from navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -77,7 +77,7 @@ const Navbar = () => {
         <div className='flex group relative items-center text-white font-montserrat font-medium '>
         <div className=' flex items-center gap-1 text-white font-montserrat font-semibold mt-0 cursor-pointer'>
          <span onClick={() => navigate("/courses")} className='text-white font-medium'>Courses</span>
-        <RiArrowDropDownLine  className='text-3xl'/>
+        {/* <RiArrowDropDownLine  className='text-3xl'/> */}
         </div>
         
         
