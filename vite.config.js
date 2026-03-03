0ebeeb664c2323addc5d7399dfaa56e7a98993c2
Shobhan Bhagwati <shobhanbhagwati2004@gmail.com>
2026-03-04 00:35:02 +0530

    fix: vite define env variable for production

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,  tailwindcss()],
-   server: {
+  plugins: [react(), tailwindcss()],
+  server: {
     port: 3000
+  },
+  define: {
+    'import.meta.env.VITE_APP_BASE_URL': JSON.stringify(process.env.VITE_APP_BASE_URL)
   }
 })
