10d4038879797bfa24a1522d2b5037ec7d4a6c3b
Shobhan Bhagwati <shobhanbhagwati2004@gmail.com>
2026-03-04 00:11:24 +0530

    fix: BASE_URL env variable set for production

--- src/services/apis.jsx
+++ src/services/apis.jsx
@@ -1,7 +1,6 @@
 // const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
-const BASE_URL = "http://localhost:4000/api/v1";
-// console.log(BASE_URL);
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
 
 // AUTH ENDPOINTS
