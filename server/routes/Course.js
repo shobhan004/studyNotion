6d3d504bb4ee0c48661e13224dd42800b1dcc25f
Shobhan Bhagwati <shobhanbhagwati2004@gmail.com>
2026-03-04 09:24:27 +0530

    fix: removed auth from getFullCourseDetails route

--- server/routes/Course.js
+++ server/routes/Course.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { auth } = require("../middlewares/auth");
-const {createCourse ,addToCart , removeFromCart , getCourses , deleteCourse , getInstructorCourses  , getAllCourses, getFullCourseDetails , editCourse} = require('../controllers/Course');
+const {createCourse ,addToCart , removeFromCart  , getCourses , deleteCourse , getInstructorCourses  , getAllCourses, getFullCourseDetails , editCourse} = require('../controllers/Course');
 const {createCategory , showAllCategories} = require('../controllers/Categories');
 const {createSubSection , deleteSubSection}   = require("../controllers/SubSection");
 const {createSection , deleteSection , updateSection} = require('../controllers/Section'); 
@@ -14,7 +14,6 @@ router.get("/getCourse" , auth , getCourses);
 router.post("/createCategory", auth, createCategory);
 router.get("/showAllCategories" , showAllCategories);
 router.get("/getAllCourses" , getAllCourses);
-
 router.get("/getInstructorCourses", auth, isInstructor, getInstructorCourses);
 // routes/Course.js
 
@@ -25,7 +24,7 @@ router.post("/addSubSection" , createSubSection);
 // createCourse ke niche ya deleteCourse ke upar ye line add karo
 router.post("/editCourse", auth, isInstructor, editCourse);
 // POST hi rakho, standard hai is project ke liye because ham courseid bhej rhe hai
-router.post("/getFullCourseDetails", auth , getFullCourseDetails);
+router.post("/getFullCourseDetails" , getFullCourseDetails);
 router.post("/updateSection", auth, isInstructor, updateSection);
 
 // DELETE SECTION
@@ -34,6 +33,7 @@ router.delete("/deleteCourse" , auth , deleteCourse);
 router.delete("/removeFromCart", auth, removeFromCart);
 router.post("/deleteSection" , auth , isInstructor , deleteSection);
 router.post("/deleteSubSection", auth, isInstructor, deleteSubSection);
+
 
 
 module.exports = router;
